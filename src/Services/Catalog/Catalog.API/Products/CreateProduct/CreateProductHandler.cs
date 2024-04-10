e14dd2f4eb7555dfc9904337200f3a4d4dc24780
Koshytec <koshytec@yahoo.com>
2024-04-10 21:40:31 +0800

    initial

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business Logic
+        throw new NotImplementedException();
+    }
+}
+
